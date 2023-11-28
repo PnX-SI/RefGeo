90919bf0ca4294393658f6df26a517baa2159d07
Élie Bouttier <elie@bouttier.eu>
2023-11-28 23:51:41 +0100

    areas: remove geojson_4326, add geom_4326
    Thus, add marshmallow schemas for serialization, notably geojson serialization.

--- src/ref_geo/env.py
+++ src/ref_geo/env.py
@@ -2,6 +2,7 @@ from os import environ
 from importlib import import_module
 
 from flask_sqlalchemy import SQLAlchemy
+from flask_marshmallow import Marshmallow
 
 
 db_path = environ.get("FLASK_SQLALCHEMY_DB")
@@ -14,4 +15,16 @@ else:
     environ["FLASK_SQLALCHEMY_DB"] = f"{__name__}.db"
 
 
-__all__ = ["db"]
+ma_path = environ.get("FLASK_MARSHMALLOW")
+if ma_path and ma_path != f"{__name__}.ma":
+    ma_module_name, ma_object_name = ma_path.rsplit(".", 1)
+    ma_module = import_module(ma_module_name)
+    ma = getattr(ma_module, ma_object_name)
+else:
+    ma = Marshmallow()
+    environ["FLASK_MARSHMALLOW"] = f"{__name__}.ma"
+    ma.SQLAlchemySchema.OPTIONS_CLASS.session = db.session
+    ma.SQLAlchemyAutoSchema.OPTIONS_CLASS.session = db.session
+
+
+__all__ = ["db", "ma"]
--- src/ref_geo/models.py
+++ src/ref_geo/models.py
@@ -56,7 +56,7 @@ class LAreas(db.Model):
     area_code = db.Column(db.Unicode)
     geom = db.Column(Geometry("MULTIPOLYGONE"))
     centroid = db.Column(Geometry("POINT"))
-    geojson_4326 = deferred(db.Column(db.Unicode))
+    geom_4326 = db.Column(Geometry("MULTIPOLYGONE", 4326))
     source = db.Column(db.Unicode)
     enable = db.Column(db.Boolean, nullable=False, default=True)
     meta_create_date = db.Column(db.DateTime, default=datetime.now)
